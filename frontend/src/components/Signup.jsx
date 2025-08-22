b27bf39c8f361ddac6e6d706ad07c61d2135ee9b
NEKAL SINGH SALARIA <166684227+nekalsalaria@users.noreply.github.com>
2025-08-22 23:46:59 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -26,7 +26,7 @@ const Signup = () => {
     setLoading(true); // Start loading
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/signup", formData);
+      const res = await axios.post("https://swapskill-com.onrender.com/api/auth/signup", formData);
 
       const { token, user } = res.data;
 
